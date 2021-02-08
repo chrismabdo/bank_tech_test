882c652b3d57d400dc940f6d9108f80f58fcdffa
Chris Abdo <christophermabdo@icloud.com>
2021-02-08 12:51:00 +0000

    added gemfile: rspec, rubocop and simplecov. Fixed existing rubocop warnings

--- lib/account.rb
+++ lib/account.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: false
+
+# this class holds information pertaining to a users' account, allows deposits, withdrawls and printing statements
 class Account
+  attr_reader :balance, :credit, :debit
 
-    attr_reader :balance, :credit, :debit
+  def initialize
+    @balance = 0
+    @credit = []
+    @debit = []
+  end
 
-    def initialize
-        @balance = 0
-        @credit = []
-        @debit = []
-
-    end
-
-    def deposit(amount)
-        @balance += amount
-    end
+  def deposit(amount)
+    @balance += amount
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,33 +1,34 @@
+# frozen_string_literal: false
+
 require 'account'
 
 describe Account do
-    before(:each) do
-        @account = Account.new
-    end
-    
-    it 'will be an instance of Account' do
-        expect(@account).to be_kind_of Account 
-    end
+  before(:each) do
+    @account = Account.new
+  end
 
-    it 'will be initialized with an empty balance' do
-        expect(@account.balance).to eq 0
-    end
+  it 'will be an instance of Account' do
+    expect(@account).to be_kind_of Account
+  end
 
-    it 'will keep records of credit' do
-        expect(@account.credit).to eq []
-    end
+  it 'will be initialized with an empty balance' do
+    expect(@account.balance).to eq 0
+  end
 
-    it 'will keep records of debit' do
-        expect(@account.debit).to eq []
-    end
+  it 'will keep records of credit' do
+    expect(@account.credit).to eq []
+  end
 
-    it 'will respond to a deposit method' do
-        expect(@account).to respond_to(:deposit).with(1).argument
-    end
+  it 'will keep records of debit' do
+    expect(@account.debit).to eq []
+  end
 
-    it 'will deposit funds to the account balance' do
-        @account.deposit(50)
-        expect(@account.balance).to eq 50
-    end
+  it 'will respond to a deposit method' do
+    expect(@account).to respond_to(:deposit).with(1).argument
+  end
 
+  it 'will deposit funds to the account balance' do
+    @account.deposit(50)
+    expect(@account.balance).to eq 50
+  end
 end

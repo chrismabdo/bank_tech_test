0eca6a20485f3808dcb10d8227327cdca24cb740
Chris Abdo <christophermabdo@icloud.com>
2021-02-08 12:36:28 +0000

    added credit, debit and balance instance variables. Tests passing for them

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,15 @@
 class Account
+
+    attr_reader :balance, :credit, :debit
+
+    def initialize
+        @balance = 0
+        @credit = []
+        @debit = []
+
+    end
+
     def deposit(amount)
+        @balance += amount
     end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,15 +3,31 @@ require 'account'
 describe Account do
     before(:each) do
         @account = Account.new
-      end
+    end
     
     it 'will be an instance of Account' do
         expect(@account).to be_kind_of Account 
+    end
+
+    it 'will be initialized with an empty balance' do
+        expect(@account.balance).to eq 0
+    end
+
+    it 'will keep records of credit' do
+        expect(@account.credit).to eq []
+    end
+
+    it 'will keep records of debit' do
+        expect(@account.debit).to eq []
     end
 
     it 'will respond to a deposit method' do
         expect(@account).to respond_to(:deposit).with(1).argument
     end
 
-    
+    it 'will deposit funds to the account balance' do
+        @account.deposit(50)
+        expect(@account.balance).to eq 50
+    end
+
 end

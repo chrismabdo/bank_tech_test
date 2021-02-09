7f8163722d1a91509695d4970c082d12db02c2c0
Chris Abdo <christophermabdo@icloud.com>
2021-02-09 10:50:27 +0000

    Dependency removed in Account upon Transaction

--- lib/account.rb
+++ lib/account.rb
@@ -10,7 +10,7 @@ class Account
     @balance = 0
     @history = []
   end
-
+# Trying to withdraw the dependency of Account on Transaction
   def deposit(amount, transaction = Transaction.new)
     @transaction = transaction
     @transaction.debit(amount)
@@ -19,20 +19,18 @@ class Account
     @history.push(transaction.log)
   end
 
-  def withdraw(amount)
-    @transaction = Transaction.new
+  def withdraw(amount, transaction = Transaction.new)
+    @transaction = transaction
     @transaction.credit(amount)
     @balance -= amount
     @transaction.log << @balance
     @history.push(transaction.log)
   end
-
+# deposit and withdraw methods a bit clunky, which is why I extracted to a separate class.. Still could be slimmed down.
   def print_statement
     result = @history.reverse.map do |t| 
       t[0] + " ||   " + t[1].to_s + t[2].to_s + " || " + t[3].to_s 
     end
-
     print "   date    || credit || debit || balance\n#{result.join("\n")}" 
-    
   end
 end
--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,5 +1,7 @@
 require 'date'
+# frozen_string_literal: false
 
+# this class holds information pertaining to a single transaction, feeding information into deposits and withdrawls.
 class Transaction
   attr_reader :log
     def initialize
@@ -17,7 +19,7 @@ class Transaction
       @log << amount
       @log << "   ||      "
     end
-
+# Not ideal adding the whitespaces on lines 13 and 20. Chose to add them for clearer readability in the printed statement.
 private
 
     def add_current_date

4ac9ad245fd491f18353300829af15f7376f1a38
Chris Abdo <christophermabdo@icloud.com>
2021-02-12 13:20:12 +0000

    all rubocop offences corrected

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -25,16 +25,7 @@ describe '#print_statement' do
     allow(Date).to receive(:today).and_return Date.new(2021, 2, 8)
   end
 
-  it 'will print a statement with each transaction on separate lines' do
-    account.deposit(50)
-    account.deposit(20)
-    expect do
-      account.print_statement
-    end.to output("   date   || credit || debit || balance
-2021-02-08||||20.00||70.00\n2021-02-08||||50.00||50.00").to_stdout
-  end
-
-  it 'will print a statement with transactions in reverse chronological order' do
+  it 'will print a statement with transactions in reverse chronological order on separate lines' do
     account.deposit(50)
     account.deposit(20)
     account.deposit(100)
